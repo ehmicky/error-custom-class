73953f1fd453123b4235ff1dcf23535a0d66daa4
ehmicky <ehmicky@gmail.com>
2022-06-05 13:25:18 +0200

    Add tests

---
+++ test/create.js
@@ -0,0 +1,69 @@
+import test from 'ava'
+import errorType from 'error-type'
+import { each } from 'test-each'
+
+const TestError = errorType('TestError')
+
+test('Sets instance options by default', (t) => {
+  const error = new TestError('test', { one: true })
+  t.true(error.one)
+})
+
+test('Can customize onCreate', (t) => {
+  const ErrorType = errorType('TestError', setOneProp)
+  const error = new ErrorType('test', { one: true })
+  t.false('one' in error)
+  t.true(error.two)
+})
+
+const setOneProp = function (error, { one }) {
+  error.two = one
+}
+
+test('Does not ignore enumerable options', (t) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  const opts = Object.defineProperty({}, 'one', {
+    value: true,
+    enumerable: true,
+  })
+  t.true(new TestError('test', opts).one)
+})
+
+test('Ignore non-enumerable options', (t) => {
+  // eslint-disable-next-line fp/no-mutating-methods
+  const opts = Object.defineProperty({}, 'one', {
+    value: true,
+    enumerable: false,
+  })
+  t.false('one' in new TestError('test', opts))
+})
+
+test('Ignore inherited options', (t) => {
+  const opts = { __proto__: { one: true } }
+  t.true(opts.one)
+  t.false('one' in new TestError('test', opts))
+})
+
+each(
+  [
+    'name',
+    'message',
+    'stack',
+    'errors',
+    'prototype',
+    'constructor',
+    '__proto__',
+    'toString',
+    'toLocaleString',
+    'hasOwnProperty',
+    'isPrototypeOf',
+    'propertyIsEnumerable',
+    'valueOf',
+  ],
+  ({ title }, propName) => {
+    test(`Ignore some instance options | ${title}`, (t) => {
+      const error = new TestError('test', { [propName]: true })
+      t.not(error[propName], true)
+    })
+  },
+)
