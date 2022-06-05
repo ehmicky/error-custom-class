b428c2da934384100b42ffc2fcd6e120d8ba71bf
ehmicky <ehmicky@gmail.com>
2022-06-05 13:32:55 +0200

    Improve types

--- src/main.d.ts
+++ src/main.d.ts
@@ -4,10 +4,9 @@
  * ```js
  * ```
  */
-export default function errorType(
+export default function errorType<
+  T extends { [instanceProperty: string | symbol]: any },
+>(
   name: string,
-  onCreate?: (error: Error, options: {}) => void,
-): new (
-  message: string,
-  options?: { cause?: any; [instanceProperty: string | symbol]: any },
-) => Error
+  onCreate?: (error: Error, options: T) => void,
+): new (message: string, options?: { cause?: any } & T) => Error
