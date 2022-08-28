1eefc467f1ab3da23e74a01138cf0a0eada7d34a
ehmicky <ehmicky@gmail.com>
2022-08-28 15:29:01 +0200

    Fix types

--- src/main.d.ts
+++ src/main.d.ts
@@ -11,61 +11,6 @@ export interface ErrorParams {
 }
 
 /**
- * Called on `new CustomError('message', params)`
- */
-export type OnCreate<
-  ErrorNameArg extends ErrorName = ErrorName,
-  ErrorParamsArg extends ErrorParams = ErrorParams,
-> = (
-  error: CustomError<ErrorNameArg, ErrorParamsArg>,
-  params: ErrorParamsArg,
-) => void
-
-/**
- * `error-custom-class` options
- */
-export interface Options<
-  ErrorNameArg extends ErrorName = ErrorName,
-  ErrorParamsArg extends ErrorParams = ErrorParams,
-> {
-  /**
-   * Called on `new CustomError('message', params)`.
-   *
-   * By default, it sets any `params.props` as `error` properties. However, you
-   * can override it with any custom logic to validate, normalize `params`, etc.
-   *
-   * @example
-   * ```js
-   * const DatabaseError = errorCustomClass('DatabaseError', {
-   *   onCreate(error, { props }) {
-   *     error.dbId = props.databaseId
-   *   },
-   * })
-   * const databaseError = new DatabaseError('message', { props: { databaseId: 2 } })
-   * console.log(databaseError.dbId) // 2
-   * console.log(databaseError.databaseId) // undefined
-   * ```
-   */
-  readonly onCreate?: OnCreate<ErrorNameArg, ErrorParamsArg>
-
-  /**
-   * Parent error class.
-   *
-   * @default Error
-   *
-   * @example
-   * ```js
-   * const ParentError = errorCustomClass('ParentError')
-   * const ChildError = errorCustomClass('ChildError', { ParentClass: ParentError })
-   * const childError = new ChildError('message')
-   * console.log(childError instanceof ChildError) // true
-   * console.log(childError instanceof ParentError) // true
-   * ```
-   */
-  readonly ParentClass?: Function
-}
-
-/**
  * Custom error class
  */
 export declare class CustomError<
@@ -97,7 +42,4 @@ export declare class CustomError<
 export default function errorCustomClass<
   ErrorNameArg extends ErrorName = ErrorName,
   ErrorParamsArg extends ErrorParams = ErrorParams,
->(
-  errorName: ErrorNameArg,
-  options?: Options<ErrorNameArg, ErrorParamsArg>,
-): typeof CustomError<ErrorNameArg, ErrorParamsArg>
+>(errorName: ErrorNameArg): typeof CustomError<ErrorNameArg, ErrorParamsArg>
--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -7,10 +7,8 @@ import {
 
 import errorCustomClass, {
   ErrorName,
-  OnCreate,
   ErrorParams,
   CustomError,
-  Options,
 } from './main.js'
 
 const TestError = errorCustomClass('TestError')
@@ -27,13 +25,7 @@ expectNotAssignable<ErrorName>('name')
 expectNotAssignable<ErrorName>(Symbol('TestError'))
 
 expectError(errorCustomClass('TestError', true))
-expectNotAssignable<Options>(true)
-
-expectNotAssignable<Options>({ onCreate: {} })
-expectNotAssignable<Options>({ onCreate(_: CustomError, __: boolean) {} })
-expectNotAssignable<Options>({ onCreate(_: boolean, __: {}) {} })
-expectAssignable<OnCreate>((_: CustomError, __: { test?: boolean }) => {})
-expectNotAssignable<OnCreate>((_: boolean) => {})
+expectError(errorCustomClass('TestError', {}))
 
 expectType<typeof CustomError<'TestError'>>(TestError)
 expectType<typeof TestError>(TestError)
@@ -48,10 +40,6 @@ new TestError('message', { cause: true })
 new TestError('message', { props: { anyProp: true } })
 new TestError('message', { props: { [Symbol('test')]: true } })
 expectError(new TestError('message', true))
-const TestErrorTwo = errorCustomClass('TestError', {
-  onCreate(_: CustomError, __: { test?: boolean }) {},
-} as const)
-expectError(new TestErrorTwo('message', { other: true }))
 expectAssignable<ErrorParams>({ anyProp: true })
 expectNotAssignable<ErrorParams>(true)
 
@@ -60,9 +48,3 @@ expectType<InstanceType<typeof TestError>>(testError)
 expectType<typeof testError>(testError)
 expectAssignable<Error>(testError)
 expectType<'TestError'>(testError.name)
-
-errorCustomClass('TestError', { ParentClass: Error })
-errorCustomClass('TestError', { ParentClass: TypeError })
-errorCustomClass('TestError', { ParentClass: TestError })
-expectError(errorCustomClass('TestError', { ParentClass: true }))
-expectNotAssignable<Options>({ ParentClass: true })
