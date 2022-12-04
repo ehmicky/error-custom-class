59b37454224c0694f96a51a1050c9326011ea805
ehmicky <ehmicky@gmail.com>
2022-12-04 19:30:41 +0100

    Update type tests

--- src/main.test-d.ts
+++ src/main.test-d.ts
@@ -1,9 +1,4 @@
-import {
-  expectAssignable,
-  expectNotAssignable,
-  expectError,
-  expectType,
-} from 'tsd'
+import { expectAssignable, expectNotAssignable, expectType } from 'tsd'
 
 import errorCustomClass, { ErrorName, CustomError } from 'error-custom-class'
 
@@ -11,36 +6,47 @@ const TestError = errorCustomClass('TestError')
 const testError = new TestError('message')
 
 errorCustomClass('TestError')
-expectError(errorCustomClass())
-expectError(errorCustomClass(true))
-expectError(errorCustomClass('name'))
-expectError(errorCustomClass(Symbol('TestError')))
+// @ts-expect-error
+errorCustomClass()
+// @ts-expect-error
+errorCustomClass(true)
+// @ts-expect-error
+errorCustomClass('name')
+// @ts-expect-error
+errorCustomClass(Symbol('TestError'))
 expectAssignable<ErrorName>('TestError')
 expectNotAssignable<ErrorName>(true)
 expectNotAssignable<ErrorName>('name')
 expectNotAssignable<ErrorName>(Symbol('TestError'))
 
-expectError(errorCustomClass('TestError', true))
-expectError(errorCustomClass('TestError', {}))
+// @ts-expect-error
+errorCustomClass('TestError', true)
+// @ts-expect-error
+errorCustomClass('TestError', {})
 
 expectType<typeof CustomError<'TestError'>>(TestError)
 expectType<typeof TestError>(TestError)
 expectNotAssignable<typeof CustomError<'TestError'>>(Error)
 expectNotAssignable<typeof CustomError>(() => {})
 
-expectError(new TestError())
-expectError(new TestError(true))
+// @ts-expect-error
+new TestError()
+// @ts-expect-error
+new TestError(true)
 
 new TestError('message', {})
 new TestError('message', { cause: true })
-expectError(new TestError('message', true))
+// @ts-expect-error
+new TestError('message', true)
 
 const InputError = errorCustomClass<'InputError', { prop: boolean }>(
   'InputError',
 )
 new InputError('message', { prop: true })
-expectError(new InputError('message', { prop: 'true' }))
-expectError(new InputError('message', { other: true }))
+// @ts-expect-error
+new InputError('message', { prop: 'true' })
+// @ts-expect-error
+new InputError('message', { other: true })
 
 expectType<CustomError<'TestError'>>(testError)
 expectType<InstanceType<typeof TestError>>(testError)
